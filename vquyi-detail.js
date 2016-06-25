4db241a68dee98e75c60b03916c8df78b0d362bb
leapar <leapar@gmail.com>
2016-06-25 17:55:30 +0800

    youku cookie处理

--- vquyi-detail.js
+++ vquyi-detail.js
@@ -18,6 +18,29 @@ function loadScript(url, callback) {
 	document.body.appendChild(script);
 }
 
+function is_youku(url) {
+	var urlReg = /(?:id_)(.*)(?:.html)/,
+		urlReg2 = /(?:v_playlist\/f)(.*)(?:o1p\d.html)/,
+		urlReg3 = /(?:youku.com\/embed\/)(.*)/,
+		match = false;
+	//prefix = 'http://v.youku.com/player/getPlayList/VideoIDS/';
+
+	if (urlReg.test(url)) {
+		match = urlReg.exec(url);
+	} else if (urlReg2.test(url)) {
+		match = urlReg2.exec(url);
+	} else if (urlReg3.test(url)) {
+		match = urlReg3.exec(url);
+	} else {
+		return false;
+	}
+
+	if (match.length === 2) {
+		return match[1];
+	} else {
+		return false;
+	}
+}
 
 function getInfo(){
 	var video = $("#video");
@@ -28,6 +51,11 @@ function getInfo(){
 	}
 	
 	var url = video.attr("src");
+	
+	var youku = is_youku(url);
+	if(youku) {
+		url = "http://v.youku.com/v_show/id_" + youku + ".html";
+	}
  
 	console.log(url);
 	console.log(JSON.stringify({url:url}));
