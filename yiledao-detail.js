09b86aa3cc46cdb9b22fd68bc1227d59600fbed0
leapar <leapar@gmail.com>
2016-09-04 12:12:45 +0800

    一乐岛

---
+++ yiledao-detail.js
@@ -0,0 +1,74 @@
+function loadScript(url, callback) {
+ 	var script = document.createElement("script");
+ 	script.type ="text/javascript";
+ 	// IE
+ 	if (script.readyState) {
+ 		script.onreadystatechange = function () {
+		 	if (script.readyState =="loaded"|| script.readyState =="complete") {
+		 		script.onreadystatechange = null;
+				callback();
+			}
+		};
+ 	} else { // others
+		script.onload = function () {
+			callback();
+		};
+	}
+	script.src = url;
+	document.body.appendChild(script);
+}
+
+function is_youku(url) {
+	var urlReg = /(?:id_)(.*)(?:.html)/,
+		urlReg2 = /(?:v_playlist\/f)(.*)(?:o1p\d.html)/,
+		urlReg3 = /(?:youku.com\/embed\/)(.*)/,
+		match = false;
+	//prefix = 'http://v.youku.com/player/getPlayList/VideoIDS/';
+
+	if (urlReg.test(url)) {
+		match = urlReg.exec(url);
+	} else if (urlReg2.test(url)) {
+		match = urlReg2.exec(url);
+	} else if (urlReg3.test(url)) {
+		match = urlReg3.exec(url);
+	} else {
+		return false;
+	}
+
+	if (match.length === 2) {
+		return match[1];
+	} else {
+		return false;
+	}
+}
+
+function getInfo(){
+	//var video = $("#video");
+
+	var host = window.location.protocol +"//"+ window.location.host;
+	if(window.location.port != '') {
+		host += ":"+window.location.port;
+	}
+	
+//	var url = video.attr("src");
+	
+//	var youku = is_youku(url);
+//	if(youku) {
+//		url = "http://v.youku.com/v_show/id_" + youku + ".html";
+//	}
+
+	var url = "http://v.youku.com/v_show/id_" + videoIdEn + ".html";
+ 
+	console.log(url);
+	console.log(JSON.stringify({url:url}));
+		
+	if (typeof(WebViewBridgeAndroid) != "undefined" && WebViewBridgeAndroid) {
+		WebViewBridgeAndroid.send(JSON.stringify({url:url,type:'detail'}));
+		// $("#info").text(JSON.stringify(e));  
+	}
+	
+}
+
+loadScript("http://libs.useso.com/js/jquery/2.0.0/jquery.min.js", function () {
+	getInfo();
+});
---
+++ yiledao.js
@@ -0,0 +1,80 @@
+function loadScript(url, callback) {
+ 	var script = document.createElement("script");
+ 	script.type ="text/javascript";
+ 	// IE
+ 	if (script.readyState) {
+ 		script.onreadystatechange = function () {
+		 	if (script.readyState =="loaded"|| script.readyState =="complete") {
+		 		script.onreadystatechange = null;
+				callback();
+			}
+		};
+ 	} else { // others
+		script.onload = function () {
+			callback();
+		};
+	}
+	script.src = url;
+	document.body.appendChild(script);
+}
+
+function getList(){
+	var ul = $("ul.mod-pic li");
+	var list = new Array();
+	var host = window.location.protocol +"//"+ window.location.host;
+	if(window.location.port != '') {
+		host += ":"+window.location.port;
+	}
+
+	for(var i = 0;i < ul.length;i++) {
+		var li = ul[i];
+		var link = $(li).find('a')[0];
+		link = $(link).attr("href");
+
+		if(link.indexOf("/") == 0) {
+			link = host + link;
+		}
+		
+		var img = $(li).find('img')[0];
+		var thumb = $(img).attr("src");
+		
+		if(thumb.indexOf("/") == 0) {
+			thumb = host + thumb;
+		}	
+			
+		var title = $(li).find('a:eq(1)')[0];
+		title = title.innerText;
+	
+		var time = $(li).find('span font')[0];
+		time = time.innerText;
+		
+	//	var rating = $(li).find('p:eq(0)')[0];
+	//	rating = rating.innerText;
+		var rating = "";
+		
+		list[i] = {
+			title : title,
+			thumb : thumb,
+			time : time,
+			link :link,
+			rating:rating,
+		}
+	}
+	
+	
+	console.log(list);
+	console.log(JSON.stringify({type:'list',data:list}));
+	
+	if (typeof(WebViewBridgeAndroid) != "undefined" && WebViewBridgeAndroid) {
+		WebViewBridgeAndroid.send(JSON.stringify({type:'list',data:list}));
+		// $("#info").text(JSON.stringify(e));  
+	} else {
+		console.log("no WebViewBridgeAndroid");
+	}
+	
+}
+
+
+loadScript("http://libs.useso.com/js/jquery/2.0.0/jquery.min.js", function () {
+	getList();
+});
