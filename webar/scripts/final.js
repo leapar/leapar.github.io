090b82d2dee66c767201c2b93c1a077e2ad501db
leapar <leapar@163.com>
2018-02-07 09:12:05 +0800

    ee

--- webar/scripts/final.js
+++ webar/scripts/final.js
@@ -289,7 +289,7 @@
               });
               p['add'](a);
               f = a;
-              f['scale']['set'](0.03, 0.03, 0.03);
+              f['scale']['set'](0.09, 0.09, 0.09);
               t = !![];
               var b = new THREE['Vector3']();
               var h = c['quaternion'];
